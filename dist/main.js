065ca5ae065aaf9882b32bc99f5a6c01c5a985b4
Dominik Schlund <ds@dominikschlund.de>
2023-11-22 17:18:10 +0100

    update naming of colors

--- dist/main.js
+++ dist/main.js
@@ -13399,14 +13399,14 @@ var yjs_default = yjs_exports;
 
 // src/editor.ts
 var usercolors = [
-  { color: "#30bced", light: "#30bced33" },
-  { color: "#6eeb83", light: "#6eeb8333" },
-  { color: "#ffbc42", light: "#ffbc4233" },
-  { color: "#ecd444", light: "#ecd44433" },
-  { color: "#ee6352", light: "#ee635233" },
-  { color: "#9ac2c9", light: "#9ac2c933" },
-  { color: "#8acb88", light: "#8acb8833" },
-  { color: "#1be7ff", light: "#1be7ff33" }
+  { dark: "#30bced", light: "#30bced33" },
+  { dark: "#6eeb83", light: "#6eeb8333" },
+  { dark: "#ffbc42", light: "#ffbc4233" },
+  { dark: "#ecd444", light: "#ecd44433" },
+  { dark: "#ee6352", light: "#ee635233" },
+  { dark: "#9ac2c9", light: "#9ac2c933" },
+  { dark: "#8acb88", light: "#8acb8833" },
+  { dark: "#1be7ff", light: "#1be7ff33" }
 ];
 var userColor = usercolors[randomUint32() % usercolors.length];
 var getOrCreateExtension = (id2, settings) => {
@@ -13415,7 +13415,7 @@ var getOrCreateExtension = (id2, settings) => {
     const undoManager = new yjs_default.UndoManager(syncData.content);
     syncData.provider.awareness.setLocalStateField("user", {
       name: settings.name,
-      color: userColor.color,
+      color: userColor.dark,
       colorLight: userColor.light
     });
     extensions[id2] = yCollab(syncData.content, syncData.provider.awareness, { undoManager });
--- src/editor.ts
+++ src/editor.ts
@@ -7,14 +7,14 @@ import Y from './yjs';
 import { extensions } from './data';
 
 export const usercolors = [
-  { color: '#30bced', light: '#30bced33' },
-  { color: '#6eeb83', light: '#6eeb8333' },
-  { color: '#ffbc42', light: '#ffbc4233' },
-  { color: '#ecd444', light: '#ecd44433' },
-  { color: '#ee6352', light: '#ee635233' },
-  { color: '#9ac2c9', light: '#9ac2c933' },
-  { color: '#8acb88', light: '#8acb8833' },
-  { color: '#1be7ff', light: '#1be7ff33' }
+  { dark: '#30bced', light: '#30bced33' },
+  { dark: '#6eeb83', light: '#6eeb8333' },
+  { dark: '#ffbc42', light: '#ffbc4233' },
+  { dark: '#ecd444', light: '#ecd44433' },
+  { dark: '#ee6352', light: '#ee635233' },
+  { dark: '#9ac2c9', light: '#9ac2c933' },
+  { dark: '#8acb88', light: '#8acb8833' },
+  { dark: '#1be7ff', light: '#1be7ff33' }
 ]
 
 export const userColor = usercolors[randomUint32() % usercolors.length]
@@ -25,7 +25,7 @@ export const getOrCreateExtension = (id: string, settings: Settings) => {
     const undoManager = new Y.UndoManager(syncData.content)
     syncData.provider.awareness.setLocalStateField('user', {
       name: settings.name,
-      color: userColor.color,
+      color: userColor.dark,
       colorLight: userColor.light
     })
     extensions[id] = yCollab(syncData.content, syncData.provider.awareness, { undoManager })
